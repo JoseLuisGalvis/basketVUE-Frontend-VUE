5caf3d7b89099d2a110c263a2812b5eb925cb4d5
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-05-22 10:41:38 -0300

    Configuracion Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/basketVUE-Frontend-VUE/", // 👈 base para GitHub Pages
   server: {
     proxy: {
       "/api": {
